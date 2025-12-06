5b2fbe6d26c96310cfd00fb87ead05f4cedd0e85
João Vitor <88534897+LoselSpt@users.noreply.github.com>
2025-12-06 01:19:06 -0300

    Revert "feat: Adiciona as tabelas medico e pessoas ao esquema do banco de dados."

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -6,15 +6,4 @@ CREATE TABLE especialidade (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(255)
-
-CREATE TABLE medico(
-  id_pessoa BIGINT PRIMARY KEY,
-  CRM VARCHAR(20),
-  especialidade_id BIGINT,
-
-Create table pessoas(
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  telefone VARCHAR(20),
-  email VARCHAR(100),
 );
